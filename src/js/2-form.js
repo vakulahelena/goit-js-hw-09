c05ba1f37e1d23ad0bd6b7ccb5d2810bd317dfdb
Helena <vakulahelena@gmail.com>
2026-03-28 19:53:53 +0200

    JS-hw-09

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,50 @@
-console.log("Form");
+const form = document.querySelector(".feedback-form");
+
+const STORAGE_KEY = "feedback-form-state";
+
+let formData = {
+  email: "",
+  message: "",
+};
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+
+  formData = parsedData;
+
+  form.elements.email.value = parsedData.email || "";
+  form.elements.message.value = parsedData.message || "";
+}
+
+
+form.addEventListener("input", onInputChange);
+
+function onInputChange(event) {
+  const { name, value } = event.target;
+
+  formData[name] = value.trim();
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert("Fill please all fields");
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+
+  formData = { email: "", message: "" };
+
+  form.reset();
+}
